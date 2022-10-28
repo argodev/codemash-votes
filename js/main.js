b295b3503945492f8d3ce5e5494ab611bcccb06d
alycit <alydiaz77@gmail.com>
2022-10-27 19:26:00 -0500

    Codemash 2023 updates

--- js/main.js
+++ js/main.js
@@ -1,258 +1,287 @@
-var uri = 'https://sessionize.com/api/v2/lo1f0aug/view/Sessions';
-var precount = 0;
-var regcount = 0;
+var uri = "https://sessionize.com/api/v2/stb7qmud/view/Sessions";
+
+const track_mapping = {
+  Architecture: "arch",
+  "Career Mapping & Development": "career",
+  Data: "data",
+  DevOps: "devops",
+  "Hardware/IoT": "hardware",
+  Mobile: "mobile",
+  "Programming Principles": "principles",
+  Security: "security",
+  "Software Quality": "quality",
+  "Teams/Leadership/The Future of Work": "teams",
+  "UI/Design": "ui",
+  "Web/Front-End": "web",
+};
+
+const validationInfo = {
+  precompilers: { selectedTalks: [], validCount: 4 },
+  arch: { selectedTalks: [], validCount: 5 },
+  career: { selectedTalks: [], validCount: 3 },
+  data: { selectedTalks: [], validCount: 5 },
+  devops: { selectedTalks: [], validCount: 5 },
+  hardware: { selectedTalks: [], validCount: 3 },
+  mobile: { selectedTalks: [], validCount: 3 },
+  principles: { selectedTalks: [], validCount: 5 },
+  security: { selectedTalks: [], validCount: 3 },
+  quality: { selectedTalks: [], validCount: 5 },
+  teams: { selectedTalks: [], validCount: 5 },
+  ui: { selectedTalks: [], validCount: 3 },
+  web: { selectedTalks: [], validCount: 5 },
+};
 
 $(document).ready(function () {
-    $.getJSON(uri)
-        .done(function (data) {
-            $('#progress').hide();
-            $.each(data[0].sessions, function (key, item) {
-                if (item.categories[0].categoryItems) {
-                  if (item.categories[0].categoryItems[0].name === 'PreCompiler') {
-                      $(formatItem(item)).appendTo($('#precompilers'));
-                  } else if (item.categories[0].categoryItems[0].name === 'General Session') {
-                      $(formatItem(item)).appendTo($('#regularsessions'));
-                  }
-                } else {
-                  console.log('failed');
-                  console.log(item);
-                }
-            });
-        });
+  $.getJSON(uri).done(function (data) {
+    json_data = data;
+    $("#progress").hide();
 
-    $('#votingform').submit(function () {
-        var pcCount = 0;
-        var regCount = 0;
-
-        // loop through all checkboxes and build an array of the 
-        // IDs for which boxes are checked.
-        var checkArray = new Array();
-        $('input[type=checkbox]').each(function () {
-            if (this.checked) {
-                checkArray.push(this.id);
-
-                // get the tag for this item
-                var tabid = $(this).parent().parent().parent().parent().prop('id');
-
-                if (tabid === "precompilers") {
-                    pcCount += 1;
-                } else if (tabid === "regularsessions") {
-                    regCount += 1;
-                }
-            }
-        });
-
-        // let's do our count/validation here (rather than on the check handler)
-        if (pcCount > 4) {
-            $("#precompModal").modal("show");
-        } else if (regCount > 15) {
-            $("#regularModal").modal("show");
-        } else {
-            // ok, all is good now... we should be able to post/save
-            var postData = { values: checkArray };
-            $.ajax({
-                type: "POST",
-                url: "https://cmsessionvotes.azurewebsites.net/api/vote",
-                data: JSON.stringify(postData),
-                contentType: "application/json; charset=utf-8",
-                processData: false,
-                success: function () {
-                    clearAllCbxs();
-                    $("#successModal").modal("show");
-                },
-                dataType: "json",
-                traditional: true
-            });
-        }
-
-        return false; // return false to cancel form action
+    //fill in counts in headers
+    $.each(validationInfo, function (trackKey, countData) {
+      $(`#${trackKey}Count`).text(
+        `${countData["selectedTalks"].length}/${countData["validCount"]}`
+      );
     });
+
+    $.each(data[0].sessions, function (key, item) {
+      if (item.categories[0].categoryItems) {
+        var sessionFormat = item.categories[0].categoryItems[0].name;
+
+        if (sessionFormat === "PreCompiler") {
+          $(formatItem(item)).appendTo($("#precompilers"));
+        } else if (sessionFormat === "General Session") {
+          var trackName = item.categories[1].categoryItems[0].name;
+          var trackId = track_mapping[trackName];
+
+          $(formatItem(item)).appendTo($("#" + trackId));
+        }
+      } else {
+        console.log("failed");
+        console.log(item);
+      }
+    });
+  });
+
+  $("#votingform").submit(function (event) {
+    event.preventDefault();
+
+    var tracksWithErrors = [];
+    // let's do our count/validation here (rather than on the check handler)
+    $.each(validationInfo, function (trackKey, countData) {
+      if (countData["selectedTalks"].length > countData["validCount"]) {
+        var trackName = Object.keys(track_mapping).find(
+          (key) => track_mapping[key] === trackKey
+        );
+        tracksWithErrors.push(`<li>${trackName}</li>`);
+      }
+    });
+
+    if (tracksWithErrors.length > 0) {
+      var errorList = tracksWithErrors.join("");
+      $("#errorList").html(errorList);
+      $("#errorModal").modal("show");
+    } else {
+      // ok, all is good now... we should be able to post/save
+      var checkArray = [];
+
+      $.each(validationInfo, function (trackKey, countData) {
+        checkArray = checkArray.concat(countData["selectedTalks"]);
+      });
+
+      var postData = { values: checkArray };
+
+      $.ajax({
+        type: "POST",
+        url: "https://cmsessionvotes.azurewebsites.net/api/vote",
+        data: JSON.stringify(postData),
+        contentType: "application/json; charset=utf-8",
+        processData: false,
+        success: function () {
+          clearAllCbxs();
+          $("#successModal").modal("show");
+        },
+        dataType: "json",
+        traditional: true,
+      });
+    }
+    return false; // return false to cancel form action
+  });
 });
 
 function clearAllCbxs() {
-    $('input[type=checkbox]').each(function () {
-        if (this.checked) {
-            // clear the checkbox
-            this.checked = false;
-            // clear the selection
-            $(this).parent().parent().removeClass("alert-primary");
-        }
-    });
+  $("input[type=checkbox]").each(function () {
+    if (this.checked) {
+      // clear the checkbox
+      this.checked = false;
+      // clear the selection
+      $(this).parent().parent().removeClass("alert-primary");
+    }
+  });
 
-    // clear/reset the counters
-    $("#pcCount").text("0/4");
-    $("#regCount").text("0/15");
+  $.each(validationInfo, function (trackKey, countData) {
+    countData["selectedTalks"] = [];
+    $(`#${trackKey}Count`).text(
+      `${countData["selectedTalks"].length}/${countData["validCount"]}`
+    );
+  });
+
+  $("#trackTabs > li:first > button").click();
 }
 
 function handleCheckmark(checkbox) {
-    // which pane is showing?
-    var $tab = $('#mainContent')
-    var $active = $tab.find('.tab-pane.active')
-    var key = $active.prop('id');
-    var ctrl = $(checkbox)
+  // which pane is showing?
+  var $tab = $("#mainContent");
+  var $active = $tab.find(".tab-pane.active");
+  var trackKey = $active.prop("id");
+  var ctrl = $(checkbox);
+  var requiredCount = validationInfo[trackKey]["validCount"];
 
-    if (key === "precompilers") {
-        var $pcSessionsPane = $("#precompilers");
-        var $checkboxes = $pcSessionsPane.find("input[type='checkbox']");
-        var $countDisplay = $("#pcCount");
-        var count = $checkboxes.filter(":checked").length;
-        $countDisplay.text(count.toString() + "/4");
+  var $trackSessionsPane = $(`#${trackKey}`);
+  var $checkboxes = $trackSessionsPane.find("input[type='checkbox']");
+  var $countDisplay = $(`#${trackKey}Count`);
+  var count = $checkboxes.filter(":checked").length;
 
-        if (count > 4) {
-            $countDisplay.removeClass("bg-primary");
-            $countDisplay.addClass("bg-danger");
-        } else {
-            $countDisplay.removeClass("bg-danger");
-            $countDisplay.addClass("bg-primary");
-        }
+  $countDisplay.text(`${count}/${requiredCount}`);
 
-        if (ctrl.prop('checked')) {
-            ctrl.parent().parent().addClass("alert-primary");
-            precount += 1;
-        } else {
-            ctrl.parent().parent().removeClass("alert-primary");
-            precount -= 1;
-        }
-    } else if (key === "regularsessions") {
-        var $regularSessionsPane = $("#regularsessions");
-        var $checkboxes = $regularSessionsPane.find("input[type='checkbox']");
-        var $countDisplay = $("#regCount");
-        var count = $checkboxes.filter(":checked").length;
-        $countDisplay.text(count.toString() + "/15");
+  if (count > requiredCount) {
+    $countDisplay.removeClass("bg-primary");
+    $countDisplay.addClass("bg-danger");
+  } else {
+    $countDisplay.removeClass("bg-danger");
+    $countDisplay.addClass("bg-primary");
+  }
 
-        if (count > 15) {
-            $countDisplay.removeClass("bg-primary");
-            $countDisplay.addClass("bg-danger");
-        } else {
-            $countDisplay.removeClass("bg-danger");
-            $countDisplay.addClass("bg-primary");
-        }
+  var currentTalkId = ctrl.attr("id");
 
-        if (ctrl.prop('checked')) {
-            ctrl.parent().parent().addClass("alert-primary");
-            regcount += 1;
-        } else {
-            ctrl.parent().parent().removeClass("alert-primary");
-            regcount -= 1;
-        }
-    }
+  if (ctrl.prop("checked")) {
+    ctrl.parent().parent().addClass("alert-primary");
+    validationInfo[trackKey]["selectedTalks"].push(currentTalkId);
+  } else {
+    ctrl.parent().parent().removeClass("alert-primary");
+
+    validationInfo[trackKey]["selectedTalks"] = $.grep(
+      validationInfo[trackKey]["selectedTalks"],
+      function (selectedTalkId) {
+        return selectedTalkId != currentTalkId;
+      }
+    );
+  }
 }
 
 function formatItem(item) {
-    // build the holding div
-    var card = $('<div />', {
-        class: 'card mb-3',
-    });
+  // build the holding div
+  var card = $("<div />", {
+    class: "card mb-3",
+  });
 
-    var row = $('<div />', {
-        class: 'row g-0'
-    });
+  var row = $("<div />", {
+    class: "row g-0",
+  });
 
-    row.appendTo(card);
+  row.appendTo(card);
 
-    var checkboxColumn1 = $('<div />', {
-        class: 'col-1 talk-checkbox bg-light'
-    });
+  var checkboxColumn1 = $("<div />", {
+    class: "col-1 talk-checkbox bg-light",
+  });
 
-    checkboxColumn1.appendTo(row);
+  checkboxColumn1.appendTo(row);
 
-    $('<input>', {
-        id: 'cbx' + item.id.toString(),
-        type: 'checkbox',
-        value: item.id,
-        onclick: 'handleCheckmark(this);',
-        class: 'form-check-input'
-    }).appendTo(checkboxColumn1);
+  $("<input>", {
+    id: item.id.toString(),
+    type: "checkbox",
+    value: item.id,
+    onclick: "handleCheckmark(this);",
+    class: "form-check-input",
+  }).appendTo(checkboxColumn1);
 
-    var contentColumn2 = $('<div />', {
-        class: 'col'
-    });
+  var contentColumn2 = $("<div />", {
+    class: "col",
+  });
 
-    contentColumn2.appendTo(row);
-    
-    var cardBody = $('<div />', {
-        class: 'card-body'
-    });
+  contentColumn2.appendTo(row);
 
-    cardBody.appendTo(contentColumn2);
-    
-    var title = $(`<h5 />`, {
-        html: item.title,
-        class: 'card-title'
-    });
+  var cardBody = $("<div />", {
+    class: "card-body",
+  });
 
-    title.appendTo(cardBody);
+  cardBody.appendTo(contentColumn2);
 
-    var speakers = $('<p />', {
-        class: 'card-text'
-    });
-    
-    // list the speakers
-    $.each(item.speakers, function (index, speaker) {
-        if (index > 0) {
-            speakers.append(', ' + speaker.name + " ");
-        } else {
-            speakers.append(speaker.name);
-        }
-    });
+  var title = $(`<h5 />`, {
+    html: item.title,
+    class: "card-title",
+  });
 
-    speakers.appendTo(cardBody);
+  title.appendTo(cardBody);
 
-    var badgeHolder = $('<p />', {
-        class: 'card-text'
-    });
+  var speakers = $("<p />", {
+    class: "card-text",
+  });
 
-    badgeHolder.appendTo(cardBody);
+  // list the speakers
+  $.each(item.speakers, function (index, speaker) {
+    if (index > 0) {
+      speakers.append(", " + speaker.name + " ");
+    } else {
+      speakers.append(speaker.name);
+    }
+  });
 
+  speakers.appendTo(cardBody);
 
-    // list the track
-    var trackInfo = item.categories.find(category => category.name === 'Track');
-    $('<span />', {
-        class: 'badge bg-primary',
-        html: trackInfo.categoryItems[0].name
+  var badgeHolder = $("<p />", {
+    class: "card-text",
+  });
+
+  badgeHolder.appendTo(cardBody);
+
+  // list the track
+  var trackInfo = item.categories.find((category) => category.name === "Track");
+  $("<span />", {
+    class: "badge bg-primary",
+    html: trackInfo.categoryItems[0].name,
+  }).appendTo(badgeHolder);
+
+  // list the level
+  var levelInfo = item.categories.find((category) => category.name === "Level");
+  $("<span />", {
+    class: "badge bg-info",
+    html: levelInfo.categoryItems[0].name,
+  }).appendTo(badgeHolder);
+
+  // list the tags
+  var tagsInfo = item.categories.find((category) => category.name === "Tags");
+  $.each(tagsInfo.categoryItems, function (key, tag) {
+    $("<span />", {
+      class: "badge bg-secondary",
+      html: tag.name,
     }).appendTo(badgeHolder);
+  });
 
-    // list the level
-    var levelInfo = item.categories.find(category => category.name === 'Level');
-    $('<span />', {
-        class: 'badge bg-info',
-        html: levelInfo.categoryItems[0].name 
-    }).appendTo(badgeHolder);
+  var showHideButton = $("<button />", {
+    class: "btn btn-dark",
+    html: "Show / Hide Abstract",
+    type: "button",
+    "data-bs-toggle": "collapse",
+    "data-bs-target": `#collapse${item.id}`,
+    "aria-expanded": "false",
+    "aria-controls": `collapse${item.id}`,
+  });
 
-    // list the tags
-    var tagsInfo = item.categories.find(category => category.name === 'Tags');
-    $.each(tagsInfo.categoryItems, function (key, tag) {
-        $('<span />', {
-            class: 'badge bg-secondary',
-            html: tag.name 
-        }).appendTo(badgeHolder);
-    });
+  showHideButton.appendTo(cardBody);
 
-    var showHideButton = $('<button />', {
-        class: 'btn btn-dark',
-        html: 'Show / Hide Abstract',
-        type: 'button',
-        'data-bs-toggle': 'collapse',
-        'data-bs-target': `#collapse${item.id}`,
-        'aria-expanded': 'false',
-        'aria-controls': `collapse${item.id}`
-    });
+  var descriptionWrapper = $("<div />", {
+    class: "collapse",
+    id: `collapse${item.id}`,
+  });
 
-    showHideButton.appendTo(cardBody);
+  descriptionWrapper.appendTo(cardBody);
 
-    var descriptionWrapper = $('<div />', {
-        class: 'collapse',
-        id: `collapse${item.id}`
-    });
+  var description = $("<p />", {
+    class: "description",
+    html: item.description,
+  });
 
-    descriptionWrapper.appendTo(cardBody);
+  description.appendTo(descriptionWrapper);
 
-    var description = $('<p />', {
-        class: 'description',
-        html: item.description
-    });
-
-    description.appendTo(descriptionWrapper);
-
-    return card;
+  return card;
 }
